e518296bd039411664c80ccbd38087ae7dea39f0
Victor Talge <vt.talge@gmail.com>
2021-06-28 19:23:20 -0300

    Imgs adicionadas

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Text, View, TextInput } from "react-native";
+
+import { styles } from "./styles";
+
+export function SignIn() {
+  const [text, setText] = useState("");
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+      <TextInput style={styles.input} onChangeText={setText} />
+      <Text>Você digitou: {text}</Text>
+    </View>
+  );
+}
