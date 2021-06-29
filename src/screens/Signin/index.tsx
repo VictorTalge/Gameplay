c84d4585fec830ba684eff8bce838b9eaecf72d7
Victor Talge <vt.talge@gmail.com>
2021-06-29 19:25:21 -0300

    Aula 1 Completa + License e Readme

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,15 +1,34 @@
-import React, { useState } from "react";
-import { Text, View, TextInput } from "react-native";
+import React from "react";
+import { Text, View, Image, StatusBar } from "react-native";
 
+import { ButtonIcon } from "../../components/ButtonIcon";
+import llustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
-  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <TextInput style={styles.input} onChangeText={setText} />
-      <Text>Você digitou: {text}</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={llustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`} favoritos com os amigos
+        </Text>
+        <ButtonIcon title="Entrar no Discord" activeOpacity={0.7} />
+      </View>
     </View>
   );
 }
